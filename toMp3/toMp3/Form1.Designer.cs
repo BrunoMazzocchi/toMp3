5e866f14147333fc3ae18309382ee292910a154f
Bruno Mazzocchi <bemazzocchi@gmail.com>
2021-06-27 17:30:22 -0600

    Works but I know that has bugs

--- toMp3/toMp3/Form1.Designer.cs
+++ toMp3/toMp3/Form1.Designer.cs
@@ -29,20 +29,136 @@ namespace toMp3
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnSearch = new MaterialSkin.Controls.MaterialButton();
+            this.btnVideo = new MaterialSkin.Controls.MaterialButton();
+            this.btnDownload = new MaterialSkin.Controls.MaterialButton();
+            this.txtURL = new MaterialSkin.Controls.MaterialTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Controls.Add(this.btnSearch);
+            this.panel1.Controls.Add(this.btnVideo);
+            this.panel1.Controls.Add(this.btnDownload);
+            this.panel1.Controls.Add(this.txtURL);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1151, 522);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnSearch.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnSearch.Depth = 0;
+            this.btnSearch.HighEmphasis = true;
+            this.btnSearch.Icon = global::toMp3.Properties.Resources.baseline_logout_black_24dp;
+            this.btnSearch.Location = new System.Drawing.Point(893, 206);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnSearch.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(64, 36);
+            this.btnSearch.TabIndex = 3;
+            this.btnSearch.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnSearch.UseAccentColor = false;
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnVideo
+            // 
+            this.btnVideo.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnVideo.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnVideo.Depth = 0;
+            this.btnVideo.HighEmphasis = true;
+            this.btnVideo.Icon = null;
+            this.btnVideo.Location = new System.Drawing.Point(655, 352);
+            this.btnVideo.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnVideo.MinimumSize = new System.Drawing.Size(158, 36);
+            this.btnVideo.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnVideo.Name = "btnVideo";
+            this.btnVideo.Size = new System.Drawing.Size(158, 36);
+            this.btnVideo.TabIndex = 2;
+            this.btnVideo.Text = "Video";
+            this.btnVideo.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnVideo.UseAccentColor = false;
+            this.btnVideo.UseVisualStyleBackColor = true;
+            this.btnVideo.Click += new System.EventHandler(this.btnVideo_Click);
+            // 
+            // btnDownload
+            // 
+            this.btnDownload.AutoSizeMode = System.Windows.Forms.AutoSizeMode.GrowAndShrink;
+            this.btnDownload.Density = MaterialSkin.Controls.MaterialButton.MaterialButtonDensity.Default;
+            this.btnDownload.Depth = 0;
+            this.btnDownload.HighEmphasis = true;
+            this.btnDownload.Icon = null;
+            this.btnDownload.Location = new System.Drawing.Point(382, 352);
+            this.btnDownload.Margin = new System.Windows.Forms.Padding(4, 6, 4, 6);
+            this.btnDownload.MinimumSize = new System.Drawing.Size(158, 36);
+            this.btnDownload.MouseState = MaterialSkin.MouseState.HOVER;
+            this.btnDownload.Name = "btnDownload";
+            this.btnDownload.Size = new System.Drawing.Size(158, 36);
+            this.btnDownload.TabIndex = 1;
+            this.btnDownload.Text = "Mp3";
+            this.btnDownload.Type = MaterialSkin.Controls.MaterialButton.MaterialButtonType.Contained;
+            this.btnDownload.UseAccentColor = false;
+            this.btnDownload.UseVisualStyleBackColor = true;
+            this.btnDownload.Click += new System.EventHandler(this.btnDownload_Click);
+            // 
+            // txtURL
+            // 
+            this.txtURL.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.txtURL.Depth = 0;
+            this.txtURL.Font = new System.Drawing.Font("Roboto", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
+            this.txtURL.LeadingIcon = null;
+            this.txtURL.Location = new System.Drawing.Point(283, 206);
+            this.txtURL.MaximumSize = new System.Drawing.Size(603, 40);
+            this.txtURL.MaxLength = 50;
+            this.txtURL.MouseState = MaterialSkin.MouseState.OUT;
+            this.txtURL.Multiline = false;
+            this.txtURL.Name = "txtURL";
+            this.txtURL.Size = new System.Drawing.Size(603, 40);
+            this.txtURL.TabIndex = 0;
+            this.txtURL.Text = "";
+            this.txtURL.TrailingIcon = null;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Montserrat", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.ForeColor = System.Drawing.Color.MidnightBlue;
+            this.label1.Location = new System.Drawing.Point(268, 96);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(689, 73);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Youtube Downloader";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1056, 475);
+            this.ClientSize = new System.Drawing.Size(1151, 522);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "toMp3";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private MaterialSkin.Controls.MaterialTextBox txtURL;
+        private MaterialSkin.Controls.MaterialButton btnVideo;
+        private MaterialSkin.Controls.MaterialButton btnDownload;
+        private MaterialSkin.Controls.MaterialButton btnSearch;
+        private System.Windows.Forms.Label label1;
     }
 }
 
--- toMp3/toMp3/Form1.cs
+++ toMp3/toMp3/Form1.cs
@@ -1,12 +1,18 @@
-﻿using System;
+﻿
+using MediaToolkit;
+using MediaToolkit.Model;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Diagnostics;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using VideoLibrary;
 
 namespace toMp3
 {
@@ -17,5 +23,157 @@ namespace toMp3
             InitializeComponent();
         }
 
+
+        private void SaveMP3(string SaveToFolder, string VideoURL, string MP3Name)
+        {
+            string source = SaveToFolder;
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(VideoURL);
+            string videopath = Path.Combine(source, vid.FullName);
+            File.WriteAllBytes(videopath, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = Path.Combine(source, vid.FullName) };
+            var outputFile = new MediaFile { Filename = Path.Combine(source, $"{MP3Name}.mp3") };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+
+                engine.Convert(inputFile, outputFile);
+            }
+
+
+            File.Delete(Path.Combine(source, vid.FullName));
+
+        }
+
+        private void SaveMP4(string SaveToFolder, string VideoURL, string MP4Name)
+        {
+            string source = SaveToFolder;
+            var youtube = YouTube.Default;
+            var vid = youtube.GetVideo(VideoURL);
+            string videopath = Path.Combine(source, vid.FullName);
+            File.WriteAllBytes(videopath, vid.GetBytes());
+
+            var inputFile = new MediaFile { Filename = Path.Combine(source, vid.FullName) };
+            var outputFile = new MediaFile { Filename = Path.Combine(source, $"{MP4Name}.mp4") };
+
+            using (var engine = new Engine())
+            {
+                engine.GetMetadata(inputFile);
+
+
+                engine.Convert(inputFile, outputFile);
+            }
+
+
+            File.Delete(Path.Combine(source, vid.FullName));
+
+        }
+        public static void GoToSite(string url)
+        {
+            var psi = new System.Diagnostics.ProcessStartInfo();
+            psi.UseShellExecute = true;
+            psi.FileName = url;
+            System.Diagnostics.Process.Start(psi);
+        }
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string URL = txtURL.Text;
+            if (URL.Equals(""))
+            {
+                MessageBox.Show(text: "Please enter a URL");
+            }
+            else
+            {
+                try
+                {
+                    GoToSite(URL);
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(text: "Error: " + ex);
+
+                    throw;
+                }
+            }
+
+
+
+        }
+
+        private void btnDownload_Click(object sender, EventArgs e)
+        {
+            string URL = txtURL.Text;
+            string name = "";
+            string path = "";
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.Filter = "|*.ext|All Files (*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+  
+
+            if (URL.Equals(""))
+                {
+                    MessageBox.Show(text: "Please enter a URL");
+                }
+         
+                else
+                {
+                    try
+                    {
+                    saveFileDialog1.ShowDialog();
+                    path = saveFileDialog1.InitialDirectory;
+                    name = saveFileDialog1.FileName;
+                    SaveMP3(path, URL, name);
+                    MessageBox.Show(text: "Su audio se esta descargando");
+
+                }
+                catch (Exception ex)
+                    {
+                        MessageBox.Show(text: "Error: " + ex);
+
+                        throw;
+                    }
+                }
+            }
+
+        private void btnVideo_Click(object sender, EventArgs e)
+        {
+            string URL = txtURL.Text;
+            string name = "";
+            string path = "";
+
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            saveFileDialog1.FilterIndex = 1;
+
+
+            if (URL.Equals(""))
+            {
+                MessageBox.Show(text: "Please enter a URL");
+            }
+
+            else
+            {
+                try
+                {
+                    saveFileDialog1.ShowDialog();
+                    path = saveFileDialog1.InitialDirectory;
+                    name = saveFileDialog1.FileName;
+                    SaveMP4(path, URL, name);
+                    MessageBox.Show(text: "Su video se esta descargando");
+
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(text: "Error: " + ex);
+
+                    throw;
+                }
+            }
+        }
     }
 }
